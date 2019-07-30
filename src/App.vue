54af0127b54020d9a619b87ca0201d4daa3526ff
liyusuo <123456>
2019-07-30 18:05:08 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    {{this.$store.state.count}}
+    {{this.$store.getters.newCount}}
+    {{this.$store.state.a.b.count}}
+    <button @click="change">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'app',
+  mounted(){
+  },
+  methods:{
+    change(){
+      this.$store.dispatch('change');
+    }
+  }
+}
+</script>
+<style>
+
+</style>
